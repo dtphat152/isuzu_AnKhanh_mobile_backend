cd7bb4f9fb482f187aef5be947cfc16102aad6ca
dtphat152 <phatb2014772@student.ctu.edu.vn>
2024-07-20 15:27:02 +0700

    cai dat xu ly loi

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ const cors = require('cors');
 
 const app = express();
 
-//routers
+const ApiError = require("./app/api-error");
+const vtpt_gc_router = require('./app/routes/vtpt_gc.route');
+
 app.get('/',(req,res)=>{
     res.send('Hello isuzu');
 })
-const vtpt_gc_router = require('./app/routes/vtpt_gc.route');
 app.use('/api/vtpt_gc', vtpt_gc_router);
+
+
+// handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+    });
+});
 
 app.use(cors());
 app.use(express.json());
