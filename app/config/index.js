a61afaad1458a0cf7a3333d08e13bd1b32b3dc85
dtphat152 <phatb2014772@student.ctu.edu.vn>
2024-07-20 20:33:49 +0700

    config database

--- app/config/index.js
+++ app/config/index.js
@@ -1,7 +1,14 @@
 const config = {
     app: {
         port: process.env.PORT || 3000,
-    }
+    },
+    db: {
+        host: 'baokhoa.ddns.net',
+        port: 3306,
+        name: 'hm_dichvu_r2',
+        password: '12345',
+        user: 'quanly',
+      }
 };
 
 module.exports = config;
--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const app = require('./app');
-
 const config = require('./app/config');
+var mysql = require('mysql');
 
-const PORT = config.app.port;
+async function startServer() {
+    try {
+        var con = mysql.createConnection({
+            host: config.db.host,
+            user: config.db.user,
+            password: config.db.password,
+            database: config.db.name
+        });
+        await con.connect();
+        console.log("Connected database!");
+        const PORT = config.app.port;
+        app.listen(PORT, () => { 
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log("Can not connected database!", error);
+        process.exit();
+    }
+};
 
-app.listen(PORT, () => { 
-    console.log(`Server is running on port ${PORT}`);
-});
+startServer();
